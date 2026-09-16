7da10247d8387a4def38e2ce3a1438a5c5c45705
maikel50 <palango4887@gmail.com>
2026-09-16 13:38:45 +0200

    añadido sitemap y  favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://michael-web.palango4887.workers.dev",
+  integrations: [
+    sitemap(),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
